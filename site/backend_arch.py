fb96fb5417c8024a2eaf3f450ef2f8fc45868384
Hch Li <hchli@Hchs-MacBook-Pro.local>
2024-02-29 18:38:41 -0600

    second

---
+++ site/backend_arch.py
@@ -0,0 +1,13 @@
+while(i< 10)
+{
+	take user input 
+
+	generate option goodness (0 to 1), give options, append three options
+
+	send it to GPT
+
+	output, write a prompt to make sure the output format. (current )
+
+	print;
+}
+
